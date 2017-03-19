e35c9186feff2f2ac11810405644b0096dd9c080
Andrei Cacio <andrei.cacio@yahoo.com>
2017-03-19 15:33:40 +0200

    Added fibonacci project

---
+++ projects/fibonacci_sequence/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+fn fibo(n: i32) -> i32 {
+	if n < 2 {
+		return n;
+	}
+
+	return fibo(n - 1) + fibo(n - 2);
+}
+
+fn main() {
+	let args = env::args().collect::<Vec<String>>();
+	let app_name = &args[0];
+	if args.len() < 2 {
+		println!("usage {:?} [number]", app_name);
+	} else {
+		let n = &args[1];
+		let nn = n.parse::<i32>().unwrap();
+		println!("{}", fibo(nn));
+	}
+}
